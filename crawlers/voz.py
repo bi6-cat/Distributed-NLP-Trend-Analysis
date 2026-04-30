949136c5c18a392f755cccd806b358507103db60
Nguyen Quang <quangnn906@gmail.com>
2026-04-30 08:32:04 +0700

    merge(crawl): update method to upload file to HDFS

--- crawlers/voz.py
+++ crawlers/voz.py
@@ -415,3 +415,12 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+    print("\n===========================================")
+    print("🚀 Bắt đầu tự động đẩy dữ liệu lên HDFS...")
+    print("===========================================")
+    try:
+        from upload_to_hdfs import main as upload_main
+        upload_main()
+    except Exception as e:
+        print(f"❌ Lỗi khi tự động tải dữ liệu lên HDFS: {e}")
